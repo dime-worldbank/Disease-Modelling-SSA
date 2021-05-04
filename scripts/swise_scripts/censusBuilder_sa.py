6cd0259fad2ecf4ea79e08688507d8389cfcdfab
spet2094 <sophie2ayling@gmail.com>
2021-05-04 14:49:28 +0100

    fixing to_csv output on census

--- scripts/swise_scripts/censusBuilder_sa.py
+++ scripts/swise_scripts/censusBuilder_sa.py
@@ -19,10 +19,10 @@ from covid19_abm.dir_manager import get_data_dir
 # define the relevant filenames
 
 #census_filename =get_data_dir('raw', 'census', 'census_sample_1500.dta')
-census_filename =get_data_dir('raw', 'census', '5_perc_sample', 'census_sample_5perc_042221.dta')#'census_sample_5perc.dta')
+census_filename =get_data_dir('raw', 'census', '5_perc_sample', 'census_sample_5perc_040521.dta')#'census_sample_5perc.dta')
 #'ABM_Simulated_Pop_WardDistributed_UpdatedMay30_school_complete_060520.dta'
 district_filename = get_data_dir('raw','district_relation.csv')
-output_filename = get_data_dir('preprocessed', 'census', 'census_sample_5perc_042221.csv')#pickle')
+output_filename = get_data_dir('preprocessed', 'census', 'census_sample_5perc_040521.csv')#pickle')
 
 # set up mappings between the input data and the values used by the census builder
 
@@ -162,7 +162,7 @@ raw_output.rename(columns={'Schoolnumber': 'school_id'}, inplace=True)
 
 print("Writing out to file...")
 
-mining_df[relevant_cols].to_pickle(output_filename)
+mining_df[relevant_cols].to_csv(output_filename)
 #raw_output.to_pickle(output_filename)
 #raw_output.to_csv(output_filename)
 
