ef4a688e6fe66564dbc6c173883dbd90f4be15e8
RobertManningSmith <58108110+RobertManningSmith@users.noreply.github.com>
2024-11-18 11:57:15 +0000

    comments and improvements to check_parameter_files....

--- analysis_scripts/myriad_running_tools/check_parameter_files_have_all_the_data.py
+++ analysis_scripts/myriad_running_tools/check_parameter_files_have_all_the_data.py
@@ -1,6 +1,6 @@
 import os
 import sys
-# define your file paths, if running from IDE specify file paths in 'if' part, otherwise specify in terminal
+# define your file paths, if running from IDE specify file paths in 'except' part, otherwise specify in terminal
 try:
     folder_path = sys.argv[1]
     parameter_files_to_check = sys.argv[2]
@@ -21,30 +21,45 @@ necessary_components_for_model_run = [
 
 
 def main():
+    # Create a quick check on the input format of the variables
+    assert folder_path[-1] == '/', "Please add '/' to the end of your folder path"
+    assert isinstance(parameter_files_to_check, list), "Please format the parameter files you want to check as a list"
+    # Create storage to store what has been found in the parameter file, firstly a list of the file names listed in
+    # the parameter file
     components_for_model_run_found = []
-    has_parameter_file_present = []
+    # Create a list of
+    has_data_dir_present = False
     # open up the file parameter text file
     for parameter_file in parameter_files_to_check:
         print("Checking data in " + parameter_file + " is in the data directory")
         with open(folder_path + parameter_file, 'r') as file:
+            # Read over each line in the file
             for line in file:  # iterate over file lines using an iterator
+                # Filter those lines that relate to parameter files by checking for ':'
                 if ':' in line:
+                    # Split the string into the 'component' part
                     component = line.split(':')[0]
                     components_for_model_run_found.append(component)
+                    # Get the filename part of this line, stripping away 'spaces' and 'enters'
                     value = line.split(':')[1].strip(' ')
                     value = value.strip('\n')
+                    # If this line in the file is just the dataDir, check that it's formatted correctly
                     if component == 'dataDir':
                         if (len(value) > 0) & (value[-1] == '/'):
-                            has_parameter_file_present.append(True)
+                            has_data_dir_present = True
+                    # If it's not the dataDir, we first need to filter out parameters found that read in a file and
+                    # ignore those that don't by checking for '.' in the value variable
                     else:
                         if '.' in value:
-                            components_for_model_run_found.append(value in os.listdir(folder_path))
                             assert value in os.listdir(folder_path), value + " is not present in the data directory"
-
+        # Check that every file name is present, if not print out the name of the missing model components
         assert set(necessary_components_for_model_run).intersection(set(components_for_model_run_found)) == \
-               set(necessary_components_for_model_run), parameter_file + " does not include every thing needed for a " \
-                                                                         "model run"
-
+               set(necessary_components_for_model_run), \
+            parameter_file + " does not include every thing needed for a model run, need the following: " + \
+            ', '.join(map(str, set(necessary_components_for_model_run).difference(components_for_model_run_found)))
+        # Check that a data directory has been listed
+        assert has_data_dir_present, "You need to include the name of the folder where all the files needed for a" \
+                                     " model run are included in the format dataDir: yourFolderPath/"
 
 
 if __name__ == "__main__":
