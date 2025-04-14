3c2ef93c81cceb5a7b8c9d03fef2177c69814786
RobertManningSmith <58108110+RobertManningSmith@users.noreply.github.com>
2025-04-14 10:12:32 +0100

    ,

--- Protecs/src/main/java/uk/ac/ucl/protecs/objects/diseases/CoronavirusInfection.java
+++ Protecs/src/main/java/uk/ac/ucl/protecs/objects/diseases/CoronavirusInfection.java
@@ -100,7 +100,7 @@ public class CoronavirusInfection extends Disease {
 					double myProb = myWorld.random.nextDouble();
 					if (!otherPerson.getDiseaseSet().containsKey(DISEASE.COVID.key) && myProb < myWorld.params.infection_beta) {
 						CoronavirusInfection inf = new CoronavirusInfection(otherPerson, this.getHost(), myWorld.infectiousFramework.getEntryPoint(), myWorld);
-						myWorld.schedule.scheduleOnce(myWorld.schedule.getTime(), myWorld.param_schedule_infecting, inf); 
+						myWorld.schedule.scheduleOnce(inf, myWorld.param_schedule_infecting);
 					}
 				}
 				return;
@@ -169,7 +169,7 @@ public class CoronavirusInfection extends Disease {
 							double myProb = myWorld.random.nextDouble();
 							if (!otherPerson.getDiseaseSet().containsKey(DISEASE.COVID.key) && myProb < myWorld.params.infection_beta) {
 								CoronavirusInfection inf = new CoronavirusInfection(otherPerson, this.getHost(), myWorld.infectiousFramework.getEntryPoint(), myWorld);
-								myWorld.schedule.scheduleOnce(myWorld.schedule.getTime(), myWorld.param_schedule_infecting, inf); 
+								myWorld.schedule.scheduleOnce(inf, myWorld.param_schedule_infecting);
 							}
 						}
 						return;
