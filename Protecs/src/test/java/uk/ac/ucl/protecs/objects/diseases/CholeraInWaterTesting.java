44b9ebfa85ff6d4fe098b9ec4b17a61b35c513e0
RobertManningSmith <58108110+RobertManningSmith@users.noreply.github.com>
2025-06-18 10:43:58 +0100

    Cholera is spread from water to humans

--- Protecs/src/test/java/uk/ac/ucl/protecs/objects/diseases/CholeraInWaterTesting.java
+++ Protecs/src/test/java/uk/ac/ucl/protecs/objects/diseases/CholeraInWaterTesting.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import uk.ac.ucl.protecs.sim.WorldBankCovid19Sim;
 import uk.ac.ucl.protecs.sim.WorldBankCovid19Sim.DISEASE;
 import uk.ac.ucl.protecs.helperFunctions.*;
+import uk.ac.ucl.protecs.objects.hosts.Person;
 import uk.ac.ucl.protecs.objects.hosts.Water;
 import uk.ac.ucl.protecs.objects.locations.Household;
 import uk.ac.ucl.protecs.objects.locations.Location.LocationCategory;
@@ -75,6 +76,27 @@ public class CholeraInWaterTesting {
 
 		}
 		Assert.assertTrue(number_of_new_infections_in_water > number_of_initial_infections_in_water);
+		}
+	
+	@Test
+	public void checkCholeraIsPickedUpFromWater() {
+		WorldBankCovid19Sim sim = HelperFunctions.CreateDummySim(paramsDir + "params_cholera_in_water.txt");
+		sim.start();
+		int number_of_initial_infections_in_humans = 0;
+
+		for (Person p: sim.agents) {
+			if (p.getDiseaseSet().containsKey(DISEASE.CHOLERA.key)) number_of_initial_infections_in_humans ++;
+
+		}
+		int numDays = 50;
+		HelperFunctions.runSimulation(sim, numDays);
+		int number_of_new_infections_in_humans = 0;
+
+		for (Person p: sim.agents) {
+			if (p.getDiseaseSet().containsKey(DISEASE.CHOLERA.key)) number_of_new_infections_in_humans ++;
+
+		}
+		Assert.assertTrue(number_of_new_infections_in_humans > number_of_initial_infections_in_humans);
 		}
 	
 }
