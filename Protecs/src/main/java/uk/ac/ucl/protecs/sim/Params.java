2ba49ddbcb9d2c5a1768a9961a4282dfa6384871
RobertManningSmith <58108110+RobertManningSmith@users.noreply.github.com>
2025-08-07 14:44:37 +0100

    Update Params.java

--- Protecs/src/main/java/uk/ac/ucl/protecs/sim/Params.java
+++ Protecs/src/main/java/uk/ac/ucl/protecs/sim/Params.java
@@ -124,7 +124,7 @@ public class Params {
 	public double criticalToRecovery_std =		6.3 * ticks_per_day;
 	
 	// probability of staying at home if having covid taken from Makinde et al. 2021 https://genus.springeropen.com/articles/10.1186/s41118-021-00130-w
-	public double covid_prob_stay_at_home_mild = 0.707;
+	public double covid_prob_stay_at_home_mild = 0.589;
 	
 	// all cause mortality parameters, currently pulled out my arse
 	public ArrayList <Integer> all_cause_death_age_params;
