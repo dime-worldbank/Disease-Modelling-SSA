d97c5951ae522b7e16fd6835ec262cc4b2624865
RobertManningSmith <58108110+RobertManningSmith@users.noreply.github.com>
2026-05-08 10:16:33 +0100

    Create ReservoirHost.java

---
+++ Protecs/src/main/java/uk/ac/ucl/protecs/objects/hosts/ReservoirHost.java
@@ -0,0 +1,19 @@
+package uk.ac.ucl.protecs.objects.hosts;
+
+import uk.ac.ucl.protecs.sim.WorldBankCovid19Sim.HOST;
+
+public class ReservoirHost extends Host {
+
+	@Override
+	public String getHostType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isOfType(HOST host) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+}
